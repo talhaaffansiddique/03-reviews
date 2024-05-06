69142730247ee6d041fc5ce23027f90124d7627b
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-05-06 10:39:32 +0500

    183. Reviews - Data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+import people from "../src/data";
 const App = () => {
+  const [index, setIndex] = useState(1);
+  const { name, job, image, text } = people[index];
+
+  console.log(name);
   return <h2>Reviews Starter</h2>;
 };
 export default App;
