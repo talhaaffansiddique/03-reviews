540347edc26dd8970c7429dae5099432acc83fdc
Talha Affan Siddique <talhaaffansiddique@gmail..com>
2024-05-07 13:35:51 +0500

    187. Reviews - Check Number

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,32 @@ const App = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
 
+  const checkNumber = (number) => {
+    if (number > people.length > 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
   const nextPerson = () => {
     setIndex((currentIndex) => {
       const newIndex = currentIndex + 1;
-      return newIndex;
+      if (newIndex > people.length - 1) {
+        return 0;
+      }
+      return checkNumber(newIndex);
     });
   };
   const prevPerson = () => {
     setIndex((currentIndex) => {
       const newIndex = currentIndex - 1;
-      return newIndex;
+      if (newIndex < 0) {
+        return people.length - 1;
+      }
+      return checkNumber(newIndex);
     });
   };
 
